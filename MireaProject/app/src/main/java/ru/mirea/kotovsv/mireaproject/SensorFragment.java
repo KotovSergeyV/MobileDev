bce331314f81515519daa2aed70d9eae356755cf
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2025-06-02 00:12:39 +0300

    Weather

--- MireaProject/app/src/main/java/ru/mirea/kotovsv/mireaproject/SensorFragment.java
+++ MireaProject/app/src/main/java/ru/mirea/kotovsv/mireaproject/SensorFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.Locale;
@@ -146,3 +147,4 @@ public class SensorFragment extends Fragment implements SensorEventListener {
 
     }
 }
+
--- MireaProject/app/src/main/java/ru/mirea/kotovsv/mireaproject/WeatherFragment.java
+++ MireaProject/app/src/main/java/ru/mirea/kotovsv/mireaproject/WeatherFragment.java
@@ -1,64 +1,175 @@
 package ru.mirea.kotovsv.mireaproject;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WeatherFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ru.mirea.kotovsv.mireaproject.databinding.FragmentWeatherBinding;
+
 public class WeatherFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentWeatherBinding binding;
 
     public WeatherFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WeatherFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WeatherFragment newInstance(String param1, String param2) {
-        WeatherFragment fragment = new WeatherFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static WeatherFragment newInstance() {
+        return new WeatherFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        binding = FragmentWeatherBinding.inflate(getLayoutInflater());
+        LinearLayout root = binding.getRoot();
+        root.setBackgroundColor(Color.argb(75, 50, 255, 170));
+
+        if (isNetworkAvailable()) {
+            new DownloadLocationTask().execute("https://ipinfo.io/json");
+        } else {
+            Toast.makeText(getActivity(), "No internet connection", Toast.LENGTH_SHORT).show();
+        }
+
+        return root;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager != null ? connectivityManager.getActiveNetworkInfo() : null;
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private class DownloadLocationTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            binding.weatherTextView.setText("Seeking location");
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadData(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                String loc = responseJson.getString("loc");
+                String[] coordinates = loc.split(",");
+                if (coordinates.length == 2) {
+                    new DownloadWeatherTask().execute(coordinates[0], coordinates[1]);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                binding.weatherTextView.setText("Failed to get location");
+            }
+        }
+    }
+
+    private class DownloadWeatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            binding.weatherTextView.setText("Loading weather");
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String latitude = params[0];
+            String longitude = params[1];
+            String url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude +
+                    "&longitude=" + longitude + "&current_weather=true";
+            try {
+                return downloadData(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject weatherJson = new JSONObject(result);
+                JSONObject currentWeather = weatherJson.getJSONObject("current_weather");
+                Log.d("WEATHER", currentWeather.toString());
+                double temperature = currentWeather.getDouble("temperature");
+                double windspeed = currentWeather.getDouble("windspeed");
+                int winddirection = currentWeather.getInt("winddirection");
+
+                String weatherInfo = String.format("Температура: %.1f°C\n" +
+                                "Скорость ветра: %.1f км/ч\n" +
+                                "Непраление ветра: %d",
+                        temperature, windspeed, winddirection);
+                binding.weatherTextView.setText(weatherInfo);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String downloadData(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(10000);
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                bos.close();
+                data = bos.toString();
+            } else {
+                data = "Error: " + connection.getResponseMessage();
+            }
+            connection.disconnect();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
     }
 }
